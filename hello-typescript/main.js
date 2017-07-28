c124e67b8dd1aba4056d604f70519d8b7268d0dc
Laura Moraes Guimarães <laura.guimaraes@cedrofinances.com.br>
2017-07-28 15:36:17 -0300

    curso angular

---
+++ hello-typescript/main.js
@@ -0,0 +1,10 @@
+var minhaVar = 'minha variável';
+function minhaFunc(x, y) {
+    return x = y;
+}
+
+var numeros = [1,2,3];
+numeros.map(function(valor){
+    return valor * 2;
+});
+numeros.map(valor => valor * 2);
